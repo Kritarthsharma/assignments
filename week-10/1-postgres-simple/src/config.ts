9f5358c1963fcd4d8588473f47678df91ea089af
Kritarth Sharma <kritarthsharma222@gmail.com>
2024-05-02 15:58:38 +0530

    Removed secrets

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,1 @@
-export const DB_URL =
-  "postgresql://trial_run_owner:sgePMBto9kZ7@ep-soft-shadow-a56az2sc.us-east-2.aws.neon.tech/assignment?sslmode=require";
+export const DB_URL = "postgresql connection url";
